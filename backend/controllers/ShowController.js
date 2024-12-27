f04b14c0ce1e6ec8d468261689366788a4ac5940
NikSuslo <136102675+NikSuslo@users.noreply.github.com>
2024-12-27 16:45:28 +0500

    ///

---
+++ backend/controllers/ShowController.js
@@ -0,0 +1,52 @@
+import Show from '../models/Show.js'
+class ShowController {
+    async create (req, res) {
+        try {
+            const post = await PostService.create(req.body)
+            res.status(200).json(post);
+        } catch (e) {
+            console.error(e);
+            res.status(500).json({ message: 'Error creating post', error: e.message });
+        }
+    } 
+
+    async getAll(req, res) {
+        try {
+            const posts = await Show.find();
+            return res.json(posts);
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    async getOne(req, res) {
+        try {
+            const post = await PostService.getOne(req.params.id);
+            return res.json(post);
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+    
+    async delete(req, res) {
+        try {
+            const {id} = req.params
+            if (!id) {
+                res.status(400).json({message: 'Id не указан.'})
+            }
+            const post = await Post.findByIdAndDelete(id);
+            return res.json(post)
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+    
+}
+
+
+
+
+
+
+export default new ShowController();
+
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,12 +1,21 @@
 import Router from 'express'
 import PostController from '../controllers/PostController.js';
+import ShowController from '../controllers/ShowController.js';
+import Show from '../models/Show.js';
 
 const router = new Router()
-
+/* Посты */
 router.post('/posts', PostController.create)
 router.get('/posts', PostController.getAll)
 router.get('/posts/:id', PostController.getOne)
 router.put('/posts', PostController.update)
 router.delete('/posts/:id', PostController.delete)
+
+/* Шоу маст гоу он */
+router.post('/show', ShowController.create)
+router.get('/show', ShowController.getAll)
+router.get('/show/:id', ShowController.getOne)
+// router.put('/show', ShowController.update)
+router.delete('/show/:id', PostController.delete)
 
 export default router;
